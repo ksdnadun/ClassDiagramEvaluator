228eb68e15205a8d79731c9e162708a4176c4339
Nadun, Dinesh <Dinesh.Nadun@Pearson.Com>
2017-07-29 19:05:06 +0530

    EndPoint exposed

---
+++ src/main/java/com/research/classdiagramevaluator/dto/ClassModelDTO.java
@@ -0,0 +1,5 @@
+package com.research.classdiagramevaluator.dto;
+
+public class ClassModelDTO {
+
+}
---
+++ src/main/java/com/research/classdiagramevaluator/rest/ClassDiagramModelResource.java
@@ -0,0 +1,22 @@
+package com.research.classdiagramevaluator.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.research.classdiagramevaluator.dto.ClassModelDTO;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ClassDiagramModelResource {
+	
+	@ResponseStatus(code = HttpStatus.ACCEPTED)
+	@PostMapping("/classmodel")	
+	public void createClassModel(@RequestBody ClassModelDTO classModelDTO){
+		
+	}
+
+}
