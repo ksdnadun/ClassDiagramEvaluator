5aec283c0ee5def4273a58be432a0d88fef9cdfa
Nadun, Dinesh <Dinesh.Nadun@Pearson.Com>
2017-07-29 17:19:33 +0530

    initial commit

---
+++ src/main/java/com/research/classdiagramevaluator/ClassDiagramEvaluatorApplication.java
@@ -0,0 +1,12 @@
+package com.research.classdiagramevaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassDiagramEvaluatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassDiagramEvaluatorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/research/classdiagramevaluator/ClassDiagramEvaluatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.research.classdiagramevaluator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClassDiagramEvaluatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
